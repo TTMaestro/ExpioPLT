1fdd4af77ca937282fe7c210b1801b5bc0886ef0
TTMaestro <novikov.giga@gmail.com>
2025-01-13 20:37:28 +0200

    src upd 1

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ButtonComponent from './components/ButtonComponent';
+
+function App() {
+  return (
+    <div className="App">
+      // ...existing code...
+      <ButtonComponent />
+      // ...existing code...
+    </div>
+  );
+}
+
+export default App;
