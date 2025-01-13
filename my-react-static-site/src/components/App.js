dc8079c14d68b54b7fd8af5795ae02ade2757871
GN <novikov.giga@gmail.com>
2025-01-13 19:36:52 +0200

    first commit

---
+++ my-react-static-site/src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Button } from '@shadcn/ui';
+import './App.css';
+
+const App = () => {
+    return (
+        <div className="App">
+            <h1>Добро пожаловать в мой статический сайт на React!</h1>
+            <Button>Нажми меня</Button>
+        </div>
+    );
+};
+
+export default App;
